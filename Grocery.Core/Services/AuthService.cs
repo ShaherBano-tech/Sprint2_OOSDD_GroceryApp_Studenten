5b45592d449a8b989e526879d1207f955bdcd775
ShaherBano-tech <s1215801@student.windesheim.nl>
2025-09-19 21:56:42 +0200

    UC6: niet af wegens een error die niet op te lossen is.

--- Grocery.Core/Services/AuthService.cs
+++ Grocery.Core/Services/AuthService.cs
@@ -13,6 +13,15 @@ namespace Grocery.Core.Services
         }
         public Client? Login(string email, string password)
         {
+            var client = _clientService.Get(email);
+            if (client != null)
+            {
+                var passwordFromClient = client.GetType().GetProperty("_password", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.GetValue(client) as string;
+                if (passwordFromClient != null && PasswordHelper.VerifyPassword(password, passwordFromClient))
+                {
+                    return client;
+                }
+            }
             //Vraag de klantgegevens [Client] op die je zoekt met het opgegeven emailadres
             //Als je een klant gevonden hebt controleer dan of het password matcht --> PasswordHelper.VerifyPassword(password, passwordFromClient)
             //Als alles klopt dan klantgegveens teruggeven, anders null
